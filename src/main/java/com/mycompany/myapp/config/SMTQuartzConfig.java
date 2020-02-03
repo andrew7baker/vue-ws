e262f23d47526623d0b6e119f9434b116fc26101
andrew <andrew@example.com>
2020-02-04 00:58:22 +0800

    定时一分钟取机器列表

--- src/main/java/com/mycompany/myapp/config/SMTQuartzConfig.java
+++ src/main/java/com/mycompany/myapp/config/SMTQuartzConfig.java
@@ -1,5 +1,6 @@
 package com.mycompany.myapp.config;
 
+import com.mycompany.myapp.service.SysDictTypeService;
 import com.mycompany.myapp.util.SmtQuartz;
 import org.quartz.*;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +8,13 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SMTQuartzConfig {
+
+    private final SysDictTypeService sysDictTypeService;
+
+    public SMTQuartzConfig(SysDictTypeService sysDictTypeService) {
+        this.sysDictTypeService = sysDictTypeService;
+    }
+
     @Bean
     public JobDetail teatQuartzDetail(){
         return JobBuilder.newJob(SmtQuartz.class).withIdentity("testQuartz").storeDurably().build();
@@ -15,7 +23,8 @@ public class SMTQuartzConfig {
     @Bean
     public Trigger testQuartzTrigger(){
         SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
-            .withIntervalInSeconds(60)  //设置时间周期单位秒
+//            .withIntervalInSeconds(30)  //设置时间周期单位秒
+            .withIntervalInMinutes(1)    //设置时间周期单位分钟
             .repeatForever();
         return TriggerBuilder.newTrigger().forJob(teatQuartzDetail())
             .withIdentity("testQuartz")
--- src/main/java/com/mycompany/myapp/util/SmtQuartz.java
+++ src/main/java/com/mycompany/myapp/util/SmtQuartz.java
@@ -1,6 +1,11 @@
 package com.mycompany.myapp.util;
 
-import com.mycompany.myapp.web.rest.AccountResource;
+import com.mycompany.myapp.domain.Production;
+import com.mycompany.myapp.domain.SysDict;
+import com.mycompany.myapp.domain.SysDictType;
+import com.mycompany.myapp.service.SysDictTypeService;
+import com.mycompany.myapp.web.rest.SysDictTypeResource;
+import org.jdbi.v3.core.Jdbi;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
@@ -8,10 +13,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 public class SmtQuartz extends QuartzJobBean {
 
     private final Logger log = LoggerFactory.getLogger(SmtQuartz.class);
+
+    private final SysDictTypeService sysDictTypeService;
+
+    public SmtQuartz(SysDictTypeService sysDictTypeService) {
+        this.sysDictTypeService = sysDictTypeService;
+    }
+
     /**
      * 执行定时任务
      * @param jobExecutionContext
@@ -20,6 +34,18 @@ public class SmtQuartz extends QuartzJobBean {
     @Override
     protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         log.info("quartz task "+new Date());
+//        Optional<SysDictType> sysDictType = sysDictTypeService.findOne(3051l);
+//        System.out.println(sysDictType.get().getCode());
+//        log.info("quartz task "+sysDictType.get().getCode());
+
+        Jdbi jdbi = Jdbi.create("jdbc:postgresql://tx:5432/smt?", "smt", "smt");
+        log.info("jdbi"+jdbi);
+        List<SysDict> list =jdbi.withHandle(handle ->
+            handle.createQuery("SELECT d.* from sys_dict d join  sys_dict_type  t on d.dic_type_id=t.id and t.code='MACHINE_CODE' ")
+                .mapToBean(SysDict.class)
+                .list());
+        log.info("list"+list);
+
         System.out.println("quartz task "+new Date());
     }
 }
