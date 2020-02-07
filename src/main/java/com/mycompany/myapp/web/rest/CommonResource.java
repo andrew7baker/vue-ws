7ae3c17f2c578bb3046229a5eb2786170c09c834
andrew <andrew@example.com>
2020-02-07 15:51:48 +0800

    文件名 like 查询

--- src/main/java/com/mycompany/myapp/web/rest/CommonResource.java
+++ src/main/java/com/mycompany/myapp/web/rest/CommonResource.java
@@ -79,22 +79,28 @@ public class CommonResource {
     public ResponseEntity getReport(@RequestBody Map<String, Object> params) {
         if(params!=null)
             log.info("prodName="+params.get("prodName").toString()+";machineCode="+params.get("machineCode").toString());
+
         Jdbi jdbi = Jdbi.create("jdbc:postgresql://tx:5432/smt?", "smt", "smt");
         String sql = "";
         if(params.get("machineCode")!="")
             sql = " and f.machine_code='"+params.get("machineCode").toString()+"'";
-        Map a = new HashMap<>();
-        String finalSql = sql;
+        if(params.get("prodName")!="")
+            sql = sql+" and f.file_name LIKE '%"+params.get("prodName").toString()+"%'";
+
+        String finalSql = "SELECT f.machine_code,f.machine_name,f.file_name,f.create_time from (SELECT r.*,p.id prd_id FROM sys_relation r join production p  on \n" +
+            "p.id=r.from_id) t  JOIN (SELECT f.*,d.name machine_name FROM sys_file_info f JOIN \n" +
+            "(SELECT * FROM sys_dict WHERE dic_type_id IN(SELECT ID from sys_dict_type WHERE code='SMT_MACHINE_TYPE')) d\n" +
+            "ON f.machine_code = d.code) f on t.to_id = f.id "
+            +sql;
+        log.info("finalSql \n"+finalSql);
         List<Map<String, Object>> list =jdbi.withHandle(handle ->
-            handle.createQuery(
-                "SELECT f.machine_code,f.machine_name,f.file_name from (SELECT r.*,p.id prd_id FROM sys_relation r join production p  on \n" +
-                    "p.id=r.from_id) t  JOIN (SELECT f.*,d.name machine_name FROM sys_file_info f JOIN \n" +
-                    "(SELECT * FROM sys_dict WHERE dic_type_id IN(SELECT ID from sys_dict_type WHERE code='SMT_MACHINE_TYPE')) d\n" +
-                    "ON f.machine_code = d.code) f on t.to_id = f.id "
-                + finalSql)
+            handle.createQuery( finalSql)
                 .mapToMap()
                 .list());
+
+        Map a = new HashMap<>();
         a.put("data",list );
+        a.put("pagesize",list );
         return ResponseEntity.ok(a);
     }
 }
